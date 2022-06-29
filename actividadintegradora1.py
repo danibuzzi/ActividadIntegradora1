23edcb6ea5362a26ae24f9e370bbe27e94e44130
BrenTorres <84297392+BTGR@users.noreply.github.com>
2022-06-29 17:13:18 -0300

    Update actividadintegradora1.py

--- actividadintegradora1.py
+++ actividadintegradora1.py
@@ -20,6 +20,16 @@ for i in range(5):
      
 
 #Suma de los numeros
+def sumar_lista(lista):
+    suma = 0
+    
+    for numero in lista:
+        suma += numero
+        
+        return suma
+ numeros = [1,2,3,4,5];
+print(sumar_lista(numeros));
+
 
 
 #Calculo del promedio
